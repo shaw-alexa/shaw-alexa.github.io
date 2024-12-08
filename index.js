9f8a2a39f8aacb0ba0c9a69a49b21848dde2d488
shaw-alexa <ajshaw125@gmail.com>
2024-12-08 17:48:13 -0500

    Projects page and About page Updates
    Included learn more information for ACUVUE® project and about me blurb. Also fixed circle design at the bottom of the page. Currently all pages should work on mobile.

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function navHome(loc) {
 }
 
 function openLink(link) {
-  
-  //window.open(link, '_blank');
+  window.open(link, '_blank');
+}
+
+function openProject(link) {
+  window.open(link, '_self');
+}
+
+function showVideo() {
+  let vid = document.getElementById("demo");
+  let btn = document.getElementById("demoBtn");
+  if (window.getComputedStyle(vid).display === "none"){
+    vid.style.display = "block";
+    btn.innerHTML = "Close Demo";
+  }
+  else {
+    vid.style.display = "none";
+    btn.innerHTML = "View Demo";
+  }
 }
